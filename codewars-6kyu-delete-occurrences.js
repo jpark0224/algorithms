528b9fa5801e0cda5e0521ed83129ba0d160e297
Julie <juliepark242@gmail.com>
2022-07-15 00:07:39 +0100

    finished codewars delete occurences answer

--- codewars-6kyu-delete-occurrences.js
+++ codewars-6kyu-delete-occurrences.js
@@ -9,17 +9,23 @@
 
 function deleteNth(arr, n) {
   const occurences = {};
+  const result = [];
   for (element of arr) {
     occurences[element] = occurences[element] + 1 || 1;
+    if (occurences[element] <= n) {
+      result.push(element);
+    }
   }
-  console.log(occurences);
+  return result;
 }
 
 console.log(deleteNth([20, 37, 20, 21], 1));
 
 // What I learned
+
 // 1. occurences.element returns a key value pair with the key "element" instead of an actual elemenet from the array.
 // This is because a dot notation is used when you know the name of the property, and [] is used when the name of the property is dynamically determined.
+
 // 2. To check if a key exists in an object, you can use an in operator as below:
 // const person = {
 //     name: 'John',
@@ -28,5 +34,12 @@ console.log(deleteNth([20, 37, 20, 21], 1));
 //   console.log('age' in person); // 👉️ false
 // details are in the below link:
 // https://bobbyhadz.com/blog/javascript-check-if-key-exists-in-object#:~:text=To%20check%20if%20a%20key,object%20or%20its%20prototype%20chain.&text=Copied!&text=The%20syntax%20when%20using%20the%20in%20operator%20is%3A%20string%20in%20object%20.
+
 // 3. To increment a value in an object, assign the value of the key to the current value + 1, e.g. obj.num = obj.num +1 || 1. If the property exists on the object, its value gets incremented by 1, and if it doesn't - it gets initialized to 1.
 // https://bobbyhadz.com/blog/javascript-increment-value-in-object#:~:text=To%20increment%20a%20value%20in,it%20gets%20initialized%20to%201%20.
+
+// 4. a filter() method can be used instead as below:
+// const deleteNth = (a, x) => {
+//     let m = {};
+//     return a.filter( v => (m[v] = m[v]+1||1) <= x );
+//   }
